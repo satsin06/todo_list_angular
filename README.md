eafc5dbb46061be1d6ad86176ca10c6a1243bc77
Satyam Sinha <satyamsinha9404@gmail.com>
2021-12-13 22:21:15 +0530

    bootstrap initiated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SatsinTodoList
+# Todo List
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.1.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'satsin-todo-list';
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.title = "ToDO";
+  //   },)
+  // }
 }
